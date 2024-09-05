1f55d6fbfaac2a74fb8271ffc21d2287afd51b0a
unknown <Zukazine>
2024-09-05 12:04:20 +0700

    SCRUM-15 go to v5

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,9 @@ export default defineConfig({
       // implement node event listeners here
       
     },
+    excludeSpecPattern : [
+      'e2e/1-getting-started/**/*', 
+      'e2e/2-advanced-examples/**/*'
+    ]
   },
 });
