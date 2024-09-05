7e3d9d15f40ac1c88f8b4b63851c407b25ac5924
unknown <Zukazine>
2024-09-05 14:27:18 +0700

    SCRUM-15 #done done all the CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
     excludeSpecPattern : [
       'e2e/1-getting-started/**/*', 
